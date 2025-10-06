2effd91f202839b55c0c455d86d9e700d29d06e0
Enzo Gerola <enzo-pg@hotmail.com>
2025-10-06 21:23:01 +0200

    feature: Adding CRUD operations to chirps + Sign up user

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,11 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    /** 
+     * Get the chirps for the user.
+     */
+    public function chirps() {
+        return $this->hasMany(Chirp::class);
+    }
 }
